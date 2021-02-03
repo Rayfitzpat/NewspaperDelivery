c22dab8537ab788e097c522a756ed54aeac178f6
Yuliia Dovbak <yulia.dovbak@gmail.com>
2021-02-03 10:57:17 +0000

    CystomerView.java started

--- src/CustomerMain.java
+++ src/CustomerMain.java
@@ -1,18 +1,34 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class CustomerMain {
-    public static void main(String[] args) {
-        Customer customer = new Customer();
 
+    // connection with the database
+    static Connection con = null;
+    static Statement stmt = null;
+    static ResultSet rs = null;
+    static Scanner in = new Scanner(System.in);
 
-
+    public static void init_db() {
         try {
-            customer.validateHoliday("2021-29", "2021-10-9");
-            System.out.println("valid");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://localhost:3306/databaseGroupProject?useTimezone=true&serverTimezone=UTC";
+            con = DriverManager.getConnection(url, "root", "admin");
+            stmt = con.createStatement();
+        } catch (Exception e) {
+            System.out.println("Error: Failed to connect to database\n" + e.getMessage());
         }
-        catch (CustomerExceptionHandler e)
-        {
-            System.out.println("invalid");
-        }
+    }
 
 
+    public static void main(String[] args) {
+        init_db();  // open the connection to the database
+
+//        ArrayList<Customer> publicationsList =  view.readAllPublications(stmt);
+//        publication.displayAllPublications(publicationsList);
     }
 }
--- src/CustomerView.java
+++ src/CustomerView.java
@@ -1,2 +1,38 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
 public class CustomerView {
+    /**
+     * The class will be handling the outputs into the console and fetching data with the database
+     */
+
+    public ArrayList<Customer> getAllCustomers(Statement stmt)
+    {
+        // array list for saving all the objects of the Customer class
+        ArrayList<Customer> customersList = new ArrayList<>();
+
+        String query = "Select * from customer";
+        ResultSet rs;
+        try {
+            rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                int spublicationId = rs.getInt("publicationId");
+                String publicationName = rs.getString("publicationName");
+                String publicationFrequency = rs.getString("publicationFrequency");
+                float publicationCost = rs.getFloat("publicationCost");
+                int stockLevel = rs.getInt("publicationStockLevel");
+
+                customersList.add(new Customer());
+            }
+
+        } catch (SQLException sqle) {
+            System.out.println("Error: failed to read all customers.");
+            System.out.println(sqle.getMessage());
+            System.out.println(query);
+        }
+
+        return customersList;
+    }
 }
